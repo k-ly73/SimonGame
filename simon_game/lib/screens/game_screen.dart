dbf6660325db705b7163dfc80e65bb2e9645a764
Kevin Ly <45864296+k-ly73@users.noreply.github.com>
2020-05-07 11:59:57 -0700

    Deleted some screens that may not be necessary

---
+++ simon_game/lib/screens/game_screen.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+class GameScreen extends StatefulWidget {
+
+  _GameScreen createState() => new _GameScreen();
+}
+
+class _GameScreen extends State<GameScreen> {
+  Widget build(BuildContext context) {
+    
+  }
+}
