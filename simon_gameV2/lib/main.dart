593484bc1e94efacde3d1d17abb7f1dda947003b
Jenson Tran <tj88268@gmail.com>
2020-05-09 16:30:49 -0700

    refactored main

--- simon_gameV2/lib/main.dart
+++ simon_gameV2/lib/main.dart
@@ -2,8 +2,5 @@ import 'package:flutter/material.dart';
 import 'components/simon.dart';
 import 'package:flutter/services.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(Simon());
-}
+void main() => runApp(Simon()); 
+
