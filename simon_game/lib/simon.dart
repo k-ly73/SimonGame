2cf16882480d7b2f883a0afc2839971592ac8822
Jenson Tran <tj88268@gmail.com>
2020-05-13 17:27:15 -0700

    added in info button

--- simon_game/lib/simon.dart
+++ simon_game/lib/simon.dart
@@ -1,6 +1,9 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'simonContainer.dart';
 import 'button.dart';
+import 'components/popup.dart';
+import 'components/popup_content.dart';
+import 'simonContainer.dart';
 import 'package:audioplayers/audio_cache.dart';
 import 'package:simon/constants.dart';
 import 'dart:math' as Math;
@@ -23,6 +26,21 @@ class ButtonColor {
 }
 
 class _SimonSaysState extends State<SimonSays> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   int levelNumber = 0;
   int simonIterator = 0;
   int userIterator = 1;
@@ -36,6 +54,219 @@ class _SimonSaysState extends State<SimonSays> {
   String gameLabel = '';
   List<int> simonSeq = [];
   Widget button;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+        title: Text('Simon Says Game'),
+      ),
+      body: Container(
+        color: Colors.blueGrey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  IconButton(
+                    icon: Image.network('https://upload.wikimedia.org/wikipedia/en/thumb/3/35/Information_icon.svg/1200px-Information_icon.svg.png'),
+                    iconSize: 35,
+                    onPressed: (){
+                      showPopup(context, _popupInfo());
+                    },
+                  )
+                ]),
+            Text(
+              '${result ? (levelNumber == 0 ? '' : 'Level $levelNumber') : gameLabel}',
+              style: TextStyle(
+                  fontFamily: 'cursive',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            heightSpacer,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                AnimatedOpacity(
+                  duration: Duration(milliseconds: 100),
+                  opacity: greenOpacity,
+                  child: SimonContainer(
+                    colour: Colors.green,
+                    getNum: Text(
+                      '1',
+                      textScaleFactor: 2,
+                    ),
+                    onPressed: () {
+                      userChosen = 1;
+                      changeOpacity(OpacityColor.green);
+                      SimonSays.play(ButtonColor.green);
+                      if (simonSeq.isNotEmpty) {
+                        if (simonSeq[simonIterator] == userChosen) {
+                          if (simonSeq.length == userIterator) {
+                            setState(() {
+                              simonIterator = 0;
+                              userIterator = 1;
+                              nextSequence();
+                            });
+                          } else {
+                            userIterator++;
+                            simonIterator++;
+                          }
+                        } else {
+                          setState(() {
+                            endGame();
+                          });
+                        }
+                      } else {
+                        setState(() {
+                          displayNullSeq();
+                        });
+                      }
+                    },
+                  ).getDecoration(),
+                ),
+              ],
+            ),
+            heightSpacer,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                AnimatedOpacity(
+                  duration: Duration(milliseconds: 100),
+                  opacity: yellowOpacity,
+                  child: SimonContainer(
+                    colour: Colors.yellow,
+                    getNum: Text(
+                      '3',
+                      textScaleFactor: 2,
+                    ),
+                    onPressed: () {
+                      userChosen = 3;
+                      changeOpacity(OpacityColor.yellow);
+                      SimonSays.play(ButtonColor.yellow);
+                      if (simonSeq.isNotEmpty) {
+                        if (simonSeq[simonIterator] == userChosen) {
+                          if (simonSeq.length == userIterator) {
+                            setState(() {
+                              simonIterator = 0;
+                              userIterator = 1;
+                              nextSequence();
+                            });
+                          } else {
+                            userIterator++;
+                            simonIterator++;
+                          }
+                        } else {
+                          setState(() {
+                            endGame();
+                          });
+                        }
+                      } else {
+                        setState(() {
+                          displayNullSeq();
+                        });
+                      }
+                    },
+                  ).getDecoration(),
+                ),
+                widthSpacer,
+                AnimatedOpacity(
+                  duration: Duration(milliseconds: 100),
+                  opacity: blueOpacity,
+                  child: SimonContainer(
+                    colour: Colors.blue,
+                    getNum: Text(
+                      '4',
+                      textScaleFactor: 2,
+                    ),
+                    onPressed: () {
+                      userChosen = 4;
+                      changeOpacity(OpacityColor.blue);
+                      SimonSays.play(ButtonColor.blue);
+                      if (simonSeq.isNotEmpty) {
+                        if (simonSeq[simonIterator] == userChosen) {
+                          if (simonSeq.length == userIterator) {
+                            setState(() {
+                              simonIterator = 0;
+                              userIterator = 1;
+                              nextSequence();
+                            });
+                          } else {
+                            userIterator++;
+                            simonIterator++;
+                          }
+                        } else {
+                          setState(() {
+                            endGame();
+                          });
+                        }
+                      } else {
+                        setState(() {
+                          displayNullSeq();
+                        });
+                      }
+                    },
+                  ).getDecoration(),
+                ),
+              ],
+            ),
+            heightSpacer,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                AnimatedOpacity(
+                  duration: Duration(milliseconds: 100),
+                  opacity: redOpacity,
+                  child: SimonContainer(
+                    colour: Colors.red,
+                    getNum: Text(
+                      '2',
+                      textScaleFactor: 2,
+                    ),
+                    onPressed: () {
+                      userChosen = 2;
+                      changeOpacity(OpacityColor.red);
+                      SimonSays.play(ButtonColor.red);
+                      if (simonSeq.isNotEmpty) {
+                        if (simonSeq[simonIterator] == userChosen) {
+                          if (simonSeq.length == userIterator) {
+                            setState(() {
+                              simonIterator = 0;
+                              userIterator = 1;
+                              nextSequence();
+                            });
+                          } else {
+                            userIterator++;
+                            simonIterator++;
+                          }
+                        } else {
+                          setState(() {
+                            endGame();
+                          });
+                        }
+                      } else {
+                        setState(() {
+                          displayNullSeq();
+                        });
+                      }
+                    },
+                  ).getDecoration(),
+                ),
+              ],
+            ),
+            heightSpacer,
+            button,
+            heightSpacer,
+          ],
+        ),
+      ),
+    );
+  }
+
 
   @override
   void initState() {
@@ -179,238 +410,60 @@ class _SimonSaysState extends State<SimonSays> {
     });
     SimonSays.player.play('wrong.mp3');
   }
-  void displayNullSeq(){
+
+  void displayNullSeq() {
     setState(() {
       gameLabel = 'Start the Game First';
     });
   }
-
-  void infoDialog(BuildContext context) {
-    var alertDialog = AlertDialog(
-      title: Text("How To Play Simon Says"),
-      content: Text("Follow Simon's lead by pressing correct buttons"),
-      actions: <Widget>[
-        FlatButton(
-          child: Text("OK"),
-          onPressed:(){}
-        )
-      ],
-    );
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alertDialog;
-      }
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.purple,
-          title: Text('Simon Says Game'),
-          actions: <Widget>[
-            
-          ],
-        ),
-        body: Container(
-          color: Colors.blueGrey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                '${result ? (levelNumber == 0 ? '' : 'Level $levelNumber') : gameLabel}',
-                style: TextStyle(
-                  fontFamily: 'cursive',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-              ),
-              heightSpacer,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  AnimatedOpacity(
-                    duration: Duration(milliseconds: 100),
-                    opacity: greenOpacity,
-                    child: SimonContainer(
-                      colour: Colors.green,
-                      getNum: Text(
-                        '1',
-                        textScaleFactor: 2,
-                      ),
-                      onPressed: () {
-                        userChosen = 1;
-                        changeOpacity(OpacityColor.green);
-                        SimonSays.play(ButtonColor.green);
-                        if (simonSeq.isNotEmpty) {
-                          if (simonSeq[simonIterator] == userChosen) {
-                            if (simonSeq.length == userIterator) {
-                              setState(() {
-                                simonIterator = 0;
-                                userIterator = 1;
-                                nextSequence();
-                              });
-                            } else {
-                              userIterator++;
-                              simonIterator++;
-                            }
-                          } 
-                          else {
-                            setState(() {
-                              endGame();
-                            });
-                          }
-                        }
-                        else {
-                          setState(() {
-                            displayNullSeq();
-                          });
-                        }
-                      },
-                    ).getDecoration(),
-                  ),
-                ],
-              ),
-              heightSpacer,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  AnimatedOpacity(
-                    duration: Duration(milliseconds: 100),
-                    opacity: yellowOpacity,
-                    child: SimonContainer(
-                      colour: Colors.yellow,
-                      getNum: Text(
-                        '3',
-                        textScaleFactor: 2,
-                      ),
-                      onPressed: () {
-                        userChosen = 3;
-                        changeOpacity(OpacityColor.yellow);
-                        SimonSays.play(ButtonColor.yellow);
-                        if (simonSeq.isNotEmpty) {
-                          if (simonSeq[simonIterator] == userChosen) {
-                            if (simonSeq.length == userIterator) {
-                              setState(() {
-                                simonIterator = 0;
-                                userIterator = 1;
-                                nextSequence();
-                              });
-                            } else {
-                              userIterator++;
-                              simonIterator++;
-                            }
-                          } else {
-                            setState(() {
-                              endGame();
-                            });
-                          }
-                        }
-                        else {
-                          setState(() {
-                            displayNullSeq();
-                          });
-                        }
-                      },
-                    ).getDecoration(),
-                  ),
-                  widthSpacer,
-                  AnimatedOpacity(
-                    duration: Duration(milliseconds: 100),
-                    opacity: blueOpacity,
-                    child: SimonContainer(
-                      colour: Colors.blue,
-                      getNum: Text(
-                        '4',
-                        textScaleFactor: 2,
-                      ),
-                      onPressed: () {
-                        userChosen = 4;
-                        changeOpacity(OpacityColor.blue);
-                        SimonSays.play(ButtonColor.blue);
-                        if (simonSeq.isNotEmpty) {
-                          if (simonSeq[simonIterator] == userChosen) {
-                            if (simonSeq.length == userIterator) {
-                              setState(() {
-                                simonIterator = 0;
-                                userIterator = 1;
-                                nextSequence();
-                              });
-                            } else {
-                              userIterator++;
-                              simonIterator++;
-                            }
-                          } else {
-                            setState(() {
-                              endGame();
-                            });
-                          }
-                        }else{
-                          setState(() {
-                            displayNullSeq();
-                          });
-                        }
-                      },
-                    ).getDecoration(),
-                  ),
-                ],
-              ),
-              heightSpacer,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  AnimatedOpacity(
-                    duration: Duration(milliseconds: 100),
-                    opacity: redOpacity,
-                    child: SimonContainer(
-                      colour: Colors.red,
-                      getNum: Text(
-                        '2',
-                        textScaleFactor: 2,
-                      ),
-                      onPressed: () {
-                        userChosen = 2;
-                        changeOpacity(OpacityColor.red);
-                        SimonSays.play(ButtonColor.red);
-                        if (simonSeq.isNotEmpty) {
-                          if (simonSeq[simonIterator] == userChosen) {
-                            if (simonSeq.length == userIterator) {
-                              setState(() {
-                                simonIterator = 0;
-                                userIterator = 1;
-                                nextSequence();
-                              });
-                            } else {
-                              userIterator++;
-                              simonIterator++;
-                            }
-                          } else {
-                            setState(() {
-                              endGame();
-                            });
-                          }
-                        }else{
-                          setState(() {
-                            displayNullSeq();
-                          });
-                        }
-                      },
-                    ).getDecoration(),
-                  ),
-                ],
-              ),
-              heightSpacer,
-              button,
-              heightSpacer,
-            ],
+  showPopup(BuildContext context, Widget widget, {BuildContext popupContext}) {
+    Navigator.push(
+      context,
+      PopupLayout(
+        top: 30,
+        left: 30,
+        right: 30,
+        bottom: 50,
+        child: PopupContent(
+          content: Scaffold(
+            appBar: AppBar(
+              title: Text('Name of your butt'),
+              leading: new Builder(builder: (context) {
+                return IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    try {
+                      Navigator.pop(context); //close the popup
+                    } catch (e) {}
+                  },
+                );
+              }),
+            ),
+            resizeToAvoidBottomPadding: false,
+            body: widget,
           ),
         ),
       ),
+    );
+  }
+
+  Widget _popupInfo() {
+    return Container(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(15, 10, 0, 0),
+                child: Text(
+                  "TLDR:",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.fromLTRB(15, 10, 0, 0),
+                  child: AutoSizeText("Butthole"))
+            ]
+        )
     );
   }
 }
